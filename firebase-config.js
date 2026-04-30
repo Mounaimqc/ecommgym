8dc2dcc8483764a9de3445bda0bca3b028b11c30
Mounaimqc <160617744+Mounaimqc@users.noreply.github.com>
2026-04-30 17:34:38 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,21 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCUcmmnvxjsZoDgQyESi5AvsynnH1kljFc",
-  authDomain: "mounir-40df8.firebaseapp.com",
-  projectId: "mounir-40df8",
-  storageBucket: "mounir-40df8.firebasestorage.app",
-  messagingSenderId: "855089551978",
-  appId: "1:855089551978:web:dfd904eb3788d3504e4813",
-  measurementId: "G-M680BM9Y77"
+  apiKey: "AIzaSyDBZaA_Ah40Edth8FIfqEeleu8s_aWjyFA",
+  authDomain: "ecomm-gym.firebaseapp.com",
+  projectId: "ecomm-gym",
+  storageBucket: "ecomm-gym.firebasestorage.app",
+  messagingSenderId: "41274656106",
+  appId: "1:41274656106:web:213f714ce5a538c35dd113",
+  measurementId: "G-32QCSS0ZBW"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-
-console.log("✅ Firebase connecté avec succès!");
-
-
-
-
+const analytics = getAnalytics(app);
